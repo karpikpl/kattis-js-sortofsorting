6d287cba4007befaa8eaad4fc81faf2c08803202
Piotr Karpala <pkarpala@infusion.com>
2017-02-09 19:09:50 -0500

    fixed logging

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-function init(toPrint, toRead) {
+/*jshint esversion: 6, node: true*/
+'use strict';
 
-    if (typeof global !== 'undefined' && (!global.print || toPrint)) {
-        global.print = toPrint || console.log;
-    }
-
-    if (typeof global !== 'undefined' && (!global.readline || toRead)) {
-        global.readline = toRead;
-    }
-}
-
+// put your solution in this method
 function solution(toPrint, toRead) {
-
-    init(toPrint, toRead);
-
-    // solution below this line
 
     const input = readline().split(' ');
     const dice1 = parseInt(input[0]);
     const dice2 = parseInt(input[1]);
 
-    //console.log(`dice1: ${dice1} dice2: ${dice2}`);
+    log(`Parsed input dice 1: ${dice1} dice 2: ${dice2}`);
 
     const results = {};
     let maxProb = 0;
@@ -34,52 +23,85 @@ function solution(toPrint, toRead) {
 
     let sums = [];
 
-    Object.keys(results).forEach((o) => sums.push({prob: results[o], sum: o}));
+    Object.keys(results).forEach((o) => sums.push({
+        prob: results[o],
+        sum: o
+    }));
 
-    sums = sums.sort((a,b) => b.prob - a.prob);
+    sums = sums.sort((a, b) => b.prob - a.prob);
     const max = sums[0].prob;
 
-    //console.log(sums);
+    //log(sums);
 
     sums
-      .filter(s => s.prob == max)
-      .sort((a,b) => a.sum - b.sum)
-      .forEach(s => print(s.sum));
+        .filter(s => s.prob == max)
+        .sort((a, b) => a.sum - b.sum)
+        .forEach(s => print(s.sum));
 }
 
+// run solution without any params for kattis
 if (typeof process === 'undefined' || process.release.name !== 'node') {
 
     solution();
 }
 
+// node js internals below -----------------------------------------------------
+
+function init(toPrint, toRead) {
+
+    // replace global functions with ones for node or tests
+    // kattis is using 'print' and 'readline' for standard I/O
+    if (typeof global !== 'undefined') {
+        global.print = toPrint;
+        global.readline = toRead;
+    }
+}
+
+// interactive mode - input from command line
 if (typeof process !== 'undefined' && process.argv[2] === 'i') {
 
     const Readline = require('readline');
-    const messages = [];
+    const input = [];
 
-    const inputProcessor = Readline.createInterface({input: process.stdin, output: process.stdout});
+    const inputProcessor = Readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-    inputProcessor.on('line', (input) => {
-        messages.push(input);
+    inputProcessor.on('line', (line) => {
 
-        if (!input) {
+        input.push(line);
+
+        if (!line) {
             inputProcessor.close();
         }
     });
 
     inputProcessor.on('close', () => {
-        console.log('closed: ' + messages);
 
-        solution(null, () => messages.shift());
+        init(console.log, () => input.shift());
+
+        solution();
     });
 }
 
+// input from process params
 if (typeof process !== 'undefined' && process.argv[2] && process.argv[2] !== 'i') {
 
-    const messages = process.argv[2].split('\n');
-    solution(null, () => messages.shift());
+    const input = process.argv[2].split('\n');
+    init(console.log, () => input.shift());
+
+    solution();
+}
+
+function log(){
+
+    if(typeof process === 'undefined' || process.release.name === 'node') {
+        console.log.call(this, ...arguments);
+    }
 }
 
 if (typeof module !== 'undefined') {
-    module.exports = solution;
+    module.exports.solution = solution;
+    module.exports.init = init;
 }
--- test/index.js
+++ test/index.js
@@ -1,11 +1,15 @@
-const solver = require('../index');
+/*jshint esversion: 6, node: true*/
+'use strict';
+
+const Index = require('../index');
 const assert = require('assert');
 
 function testSolution(input) {
 
     const result = [];
 
-    solver((ans) => result.push(ans), () => input.shift());
+    Index.init((ans) => result.push(ans), () => input.shift());
+    Index.solution();
 
     return result.length == 1 && result[0] || result;
 }
@@ -14,28 +18,26 @@ describe('Solution', function() {
 
     describe('program', function() {
 
-        [
-            {
-                input: ['6 6'],
-                result: 7
-            }, {
-                input: ['6 4'],
-                result: ['5', '6', '7']
-            }, {
-                input: ['12 20'],
-                result: [
-                    '13',
-                    '14',
-                    '15',
-                    '16',
-                    '17',
-                    '18',
-                    '19',
-                    '20',
-                    '21'
-                ]
-            }
-        ].forEach((testCase) => {
+        [{
+            input: ['6 6'],
+            result: 7
+        }, {
+            input: ['6 4'],
+            result: ['5', '6', '7']
+        }, {
+            input: ['12 20'],
+            result: [
+                '13',
+                '14',
+                '15',
+                '16',
+                '17',
+                '18',
+                '19',
+                '20',
+                '21'
+            ]
+        }].forEach((testCase) => {
 
             it('should solve for ' + testCase.input, function() {
 
