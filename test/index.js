833ff2a9b9b6336fc9e6d25ab626d2ee33d82c95
Piotr Karpala <karpik.pl@gmail.com>
2017-02-16 15:56:38 -0500

    changed default test result to array

--- test/index.js
+++ test/index.js
@@ -11,33 +11,35 @@ function testSolution(input) {
     Index.init((ans) => result.push(ans), () => input.shift());
     Index.solution();
 
-    return result.length == 1 && result[0] || result;
+    return result;
 }
 
 describe('Solution', function() {
 
     describe('program', function() {
 
-        [{
-            input: ['6 6'],
-            result: 7
-        }, {
-            input: ['6 4'],
-            result: ['5', '6', '7']
-        }, {
-            input: ['12 20'],
-            result: [
-                '13',
-                '14',
-                '15',
-                '16',
-                '17',
-                '18',
-                '19',
-                '20',
-                '21'
-            ]
-        }].forEach((testCase) => {
+        [
+            {
+                input: ['6 6'],
+                result: [7]
+            }, {
+                input: ['6 4'],
+                result: ['5', '6', '7']
+            }, {
+                input: ['12 20'],
+                result: [
+                    '13',
+                    '14',
+                    '15',
+                    '16',
+                    '17',
+                    '18',
+                    '19',
+                    '20',
+                    '21'
+                ]
+            }
+        ].forEach((testCase) => {
 
             it('should solve for ' + testCase.input, function() {
 
